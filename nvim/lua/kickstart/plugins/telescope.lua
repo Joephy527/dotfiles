e36e5edd129c3607e1fc88ea64d3f7c00e475e2a
joephy527 <joephy527@gmail.com>
2025-02-28 19:49:21 +0000

    Add git functionality

--- nvim/lua/kickstart/plugins/telescope.lua
+++ nvim/lua/kickstart/plugins/telescope.lua
@@ -75,8 +75,8 @@ return {
       }
 
       -- Enable Telescope extensions if they are installed
-      pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
+      require('telescope').load_extension 'fzf'
+      require('telescope').load_extension 'ui-select'
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
